f1220ba5f5d2b977a6c6ed2f7c42643fbeafc20c
Qian Chen <oda_san@hotmail.com>
2017-01-13 15:14:04 -0800

    ejs/handlebars

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ app.use( express.static(__dirname + '/public' ));
 app.set('view engine', 'ejs'); // res.render(file) => '/views/file'
 
 
+app.get('/list', function(req, res) {
+  var locals = {
+    message: 'Hello from Views',
+    list: list
+  };
+  res.render('list', locals);
+});
+
+// define a route that returns JSON of the list info
+// GET '/list.json' => ['Mulder', 'Scully'];
+
+var list = ['Mulder', 'Scully'];
+
+app.get('/list.json', function(req, res) {
+  res.json(list);
+});
+
+
 var port = 3000;
 app.listen(port, function(){
   console.log('Up!');
